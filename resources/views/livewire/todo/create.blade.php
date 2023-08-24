b69fac0be0646480410a1fb9a02ec5458a891718
RafaelBlum <Rafaelblum_digital@hotmail.com>
2023-08-24 01:09:06 -0300

    feat: dark mode

--- resources/views/livewire/todo/create.blade.php
+++ resources/views/livewire/todo/create.blade.php
@@ -1,11 +1,11 @@
 <div>
     {{--  --}}
     <div class="mt-4">
-        <input class="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-grey-darker" placeholder="Descrição da tarefa"
+        <input class="shadow dark:text-white appearance-none border rounded w-full py-2 px-3 mr-4 text-grey-darker dark:bg-gray-300" placeholder="Descrição da tarefa"
         wire:model.defer="title" wire:keydown.enter="save">
 
         @error('title')
-            <span class="flex items-center font-medium tracking-wide text-red-500 text-xs mt-1 ml-1">
+            <span class="flex items-center font-medium tracking-wide text-red-500 text-xs mt-1 ml-1 dark:text-white">
                 {{$message}}
             </span>
         @enderror
--- resources/views/todo-task.blade.php
+++ resources/views/todo-task.blade.php
@@ -12,45 +12,65 @@
     <!--========== ICON REMIXICON ==========-->
     <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
 
-
     <!--========== VITE css app ==========-->
     @vite('resources/css/app.css')
 </head>
-<body class="h-14 bg-gradient-to-r from-violet-200 to-fuchsia-100">
-    <div class="min-h-screen flex items-center justify-center">
-        <div class="max-w-md w-full p-5 bg-white rounded-lg shadow-lg">
+<body>
 
-            <!--========== title ==========-->
-            <h1 class="text-3xl font-bold text-center text-gray-600 mt-1 mb-1 justify-center align-content-center">Lista de tarefas
-                <div class="sm:w-7 sm:h-20 h-10 w-10 sm:mr-10 inline-flex items-center justify-center flex-shrink-3">
-                    <img src="{{asset("/images/livewire_avatar.png")}}" class="animate-bounce"/>
+
+    <!--================== TODO_ TASK - LIGTH MOD body => bg-gradient-to-r from-violet-200 to-fuchsia-100 | dark mod card =>bg-black opacity-80 ==================-->
+    <div class='flex items-center justify-center border-violet-500/25 min-h-screen bg-indigo-300 dark:bg-gray-700 dark:text-white'>
+        <div class='w-full max-w-lg px-5 py-4 mx-auto bg-white rounded-lg shadow-xl dark:bg-gray-500'>
+
+            {{-- DARK MOD --}}
+            <div class="flex flex-row-reverse text-3xl">
+                <i class="sun ri-sun-line display-none cursor-pointer"></i>
+                <i class="moon ri-sun-cloudy-fill cursor-pointer"></i>
+            </div>
+
+            <div class='max-w-md mx-auto space-y-1 dark:bg-gray-500'>
+                <div class="py-4 px-2 mx-auto max-w-screen-xl lg:py-8 lg:px-3">
+                    <div class="mx-auto max-w-screen-sm">
+
+                        <h1 class="text-3xl font-bold text-center text-gray-600 mt-1 mb-1 justify-center align-content-center dark:text-white">Lista de tarefas
+                            <div class="sm:w-7 sm:h-10 h-10 w-5 sm:mr-5 inline-flex items-center justify-center flex-shrink-1">
+                                <img src="{{asset("/images/livewire_avatar.png")}}" class="animate-bounce"/>
+                            </div>
+
+                        </h1>
+
+
+                        <!--========== call component livewire ==========-->
+                        <livewire:todo/>
+
+                        <!--==========  ==========-->
+                        <hr class="my-1 border-gray-200 sm:mx-auto dark:border-gray-100 lg:my-4">
+                        <!--==========  ==========-->
+                        <div class="sm:flex sm:items-center sm:justify-between dark:text-white">
+                                <span class="text-sm text-gray-500 sm:text-center dark:text-white">
+                                    &copy; 2023 Lista de tarefas
+                                </span>
+                            <div class="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
+                                <a href="https://www.youtube.com/channel/UCMvtn8HZ12Ud-sdkY5KzTog" target="_blank" class="text-gray-500 hover:text-gray-900 dark:text-white">
+                                    <i class="ri-youtube-fill text-3xl"></i>
+                                </a>
+                                <a href="https://www.instagram.com/universo_code" target="_blank" class="text-gray-500 hover:text-gray-900 dark:text-white">
+                                    <i class="ri-instagram-fill text-3xl"></i>
+                                </a>
+                                <a href="https://www.linkedin.com/in/rafael-blum-378656285" target="_blank" class="text-gray-500 hover:text-gray-900 dark:text-white">
+                                    <i class="ri-linkedin-box-fill text-3xl"></i>
+                                </a>
+                                <a href="https://github.com/RafaelBlum" target="_blank" class="text-gray-500 hover:text-gray-900 dark:text-white">
+                                    <i class="ri-github-fill text-3xl"></i>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </h1>
 
-            <!--========== call component livewire ==========-->
-            <livewire:todo/>
 
-            <!--==========  ==========-->
-            <hr class="my-1 border-gray-200 sm:mx-auto dark:border-gray-100 lg:my-4">
+                <div class="flex items-center justify-center text-center">
 
-            <!--==========  ==========-->
-            <div class="sm:flex sm:items-center sm:justify-between">
-                <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-                    &copy; 2023 Lista de tarefas
-                </span>
-                <div class="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
-                    <a href="https://www.youtube.com/channel/UCMvtn8HZ12Ud-sdkY5KzTog" target="_blank" class="text-gray-500 hover:text-gray-900 dark:hover:text-gray-600">
-                        <i class="ri-youtube-fill text-3xl"></i>
-                    </a>
-                    <a href="https://www.instagram.com/universo_code" target="_blank" class="text-gray-500 hover:text-gray-900 dark:hover:text-gray-600">
-                        <i class="ri-instagram-fill text-3xl"></i>
-                    </a>
-                    <a href="https://www.linkedin.com/in/rafael-blum-378656285" target="_blank" class="text-gray-500 hover:text-gray-900 dark:hover:text-gray-600">
-                        <i class="ri-linkedin-box-fill text-3xl"></i>
-                    </a>
-                    <a href="https://github.com/RafaelBlum" target="_blank" class="text-gray-500 hover:text-gray-900 dark:hover:text-gray-600">
-                        <i class="ri-github-fill text-3xl"></i>
-                    </a>
                 </div>
             </div>
         </div>
@@ -60,6 +80,60 @@
     <livewire:scripts />
     <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.8.2/dist/alpine.min.js" defer></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+
+
+    <script>
+        const sunIcon = document.querySelector('.sun');
+        const moonIcon = document.querySelector('.moon');
+
+        const userTheme = localStorage.getItem("theme");
+        const systemTheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+        console.log(sunIcon);
+        console.log(moonIcon);
+        console.log(userTheme);
+        console.log(systemTheme);
+
+        const iconToggle = () => {
+          moonIcon.classList.toggle("display-none");
+          sunIcon.classList.toggle("display-none");
+        };
+
+        const themeCheck = () => {
+            if(userTheme === 'dark' || (!userTheme && systemTheme)){
+                document.documentElement.classList.add('dark');
+                moonIcon.classList.add('display-none');
+                sunIcon.classList.remove('display-none');
+                return;
+            }
+
+            sunIcon.classList.add('display-none');
+        };
+
+        const themeSwitch = () => {
+            if(document.documentElement.classList.contains("dark")){
+                document.documentElement.classList.remove('dark');
+                localStorage.setItem("theme", "light");
+                iconToggle();
+                return;
+            }
+
+            document.documentElement.classList.add('dark');
+            localStorage.setItem("theme", "dark");
+            iconToggle();
+        };
+
+        sunIcon.addEventListener("click", () =>{
+            themeSwitch();
+        });
+
+        moonIcon.addEventListener("click", () =>{
+            themeSwitch();
+        });
+
+        // themeCheck();
+
+    </script>
 
     <script type="text/javascript" rel="script">
         $('body').on('click', '[data-editable]', function () {
